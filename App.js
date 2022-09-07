9cab89a697cc645ef3dd80323b36ddd112ea4123
RutujaVolvoikar <113047674+RutujaVolvoikar@users.noreply.github.com>
2022-09-07 23:34:32 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1><font color="red"></font>Hello This  is my First React Project</h1>
+      <p> React is a free and open-source front-end JavaScript library for building user interfaces based 
+        on UI components. It is maintained by Meta (formerly Facebook) and a community of individual 
+        developers and companies. React can be used as a base in the
+          development of single-page, mobile, or server-rendered applications with frameworks like Next.js.
+           However, React is only concerned with state management and rendering that state to the DOM, so
+            creating React applications usually requires the use of additional libraries for routing, as
+             well as certain client-side functionality.</p>
+      
+      </div>
+  );
+}
+
+export default App;
